a4849d26d287f44745db0ed6841c7ad901fe92d7
nskope-MPM <115712013+nskope-MPM@users.noreply.github.com>
2024-03-08 07:53:49 +0100

    Init Topdesk integration

---
+++ notifier_itsm_beta/CHANGELOG.md
@@ -0,0 +1,15 @@
+# 1.1.2
+## Added
+- Added TOPdesk within the notifier module
+
+# 1.1.0
+## Added
+- Added feature to dynamically add recipients.
+
+# 1.0.1
+## Changed
+- Changed default mappings.
+
+# 1.0.0
+## Added
+- Initial release.
---
+++ notifier_itsm_beta/lib/notifiers/providers/topdesk.py
@@ -0,0 +1,48 @@
+from ..core import Provider, Response
+from ..utils import requests
+import json
+
+class Topdesk(Provider):
+    """Create TOPdesk Incidents"""
+
+    base_url = "https://{domain}/tas/api/incidents"
+    site_url = "https://developers.topdesk.com"
+    name = "topdesk"
+
+    _required = {"required": ["api_key", "message"]}
+    _schema = {
+        "type": "object",
+        "properties": {
+            "domain": {"type": "string", "minLength": 1, "title": "TOPdesk SaaS URL (i.e. europe-demo-100.topdesk.net)"},
+            "username": {"type": "string", "title": "Username of the API Key}"},
+            "api_key": {"type": "string", "title": "your user API key"},
+            "message": {"type": "string", "title": "your message"},
+        },
+        "additionalProperties": False,
+    }
+
+    def _prepare_data(self, data: dict) -> dict:
+        data['text'] = data.pop("message")
+        return data
+
+    def _send_notification(self, data: dict) -> Response:
+
+        username = str(data.pop("username"))
+        api_key = str(data.pop("api_key"))
+        domain = data.pop("domain")
+        url_topdesk = self.base_url.format(domain=domain)
+       
+        message_json = data['text']
+
+        path_to_errors = ("message",)
+
+        response, errors = requests.post(
+            url_topdesk,
+            auth=(username, api_key),
+            headers={
+        "Content-Type": "application/json; charset=utf-8"
+            },
+            data=message_json,
+            path_to_errors=path_to_errors
+        )
+        return self.create_response(data, response, errors)
